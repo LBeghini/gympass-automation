aa740defc64737c49c6d1b009861283158cbcfc1
LBeghini <lavinia.beghini@gmail.com>
2022-06-01 00:31:48 -0300

    Refactor variable name

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,8 +1,8 @@
 export let convertTimeToInt = (time) => {
   let splittedTime = time.toString().split('h');
 
-  var hours = splittedTime[0];
-  let seconds = !splittedTime[1] ? '00' : splittedTime[1];
+  let hours = splittedTime[0];
+  let minutes = !splittedTime[1] ? '00' : splittedTime[1];
 
-  return parseInt(hours + seconds);
+  return parseInt(hours + minutes);
 };
